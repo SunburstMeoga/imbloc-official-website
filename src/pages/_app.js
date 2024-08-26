63032b39312daec3928b749aaff687157d2486d0
SunburstMeoga <1025113986@qq.com>
2024-08-26 15:17:40 +0800

    first commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,17 @@
 import "@/styles/globals.css";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    const handleResize = () => {
+      const e = document.documentElement.clientWidth;
+      document.getElementsByTagName('html')[0].style['font-size'] = e > 1024 ? '0.8vw' : '16px';
+    };
+    // Call it once to set initial font-size
+    handleResize();
+    // Attach the event listener to handle window resize
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, [])
   return <Component {...pageProps} />;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,25 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    spacing: Array.from({ length: 1000 }).reduce((map, _, index) => {
+      const value = ((index + 1) / 10).toFixed(1);
+      const [integerPart, decimalPart] = value.split('.');
+      const key = `${integerPart}-${decimalPart}`;
+      const formattedValue = `${integerPart}.${decimalPart}`;
+      map[key] = `${formattedValue}rem`;
+      return map;
+    }, {}),
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {},
+      fontSize: ({ theme }) => ({
+        ...theme("spacing"),
+      }),
+      lineHeight: ({ theme }) => ({
+        ...theme("spacing"),
+      }),
     },
   },
   plugins: [],
