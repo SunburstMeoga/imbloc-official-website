6308a2c305f169a25ea28d7f3410dd43dc88a081
SunburstMeoga <1025113986@qq.com>
2024-08-31 12:45:19 +0800

    language button

--- src/pages/menuBar.js
+++ src/pages/menuBar.js
@@ -7,8 +7,8 @@ const MenuBar = () => {
                 <div className='w-7-9 lg:w-9-8'>
                     <img src='/images/phone/logo.png'></img>
                 </div>
-                <div className='rounded-xl px-1-2 py-0-2 text-white bg-language-gradient-radial text-1-0 font-semibold lg:hidden'>Eng</div>
-                <div className='rounded-full cursor-pointer w-10-8 h-3-3 hidden lg:flex justify-center items-center bg-white text-module-word text-1-2  xl:w-10-8 xl:h-3-3 xl:text-1-3'>ENG / 中文</div>
+                {/* <div className='rounded-xl px-1-2 py-0-2 text-white bg-language-gradient-radial text-1-0 font-semibold lg:hidden'>Eng</div>
+                <div className='rounded-full cursor-pointer w-10-8 h-3-3 hidden lg:flex justify-center items-center bg-white text-module-word text-1-2  xl:w-10-8 xl:h-3-3 xl:text-1-3'>ENG / 中文</div> */}
             </div>
         </div>
     )
